e2251fa662748807da3b2fa71839f10f6b32af85
jarsbe <jarsbe@gmail.com>
2020-07-09 22:51:03 +1200

    Add express.

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+const app = express();
+
+app.get("/", (req,res) => {
+  res.send({ ok: true })
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(process.env.PORT || 3000);
